9c1d85ea1a25d8d4daa85e1abb312914d59d6b82
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-18 14:48:59 +0700

    scoped

--- 02-programming-in-python/function.py
+++ 02-programming-in-python/function.py
@@ -1,4 +1,4 @@
-def sum(x: int, y: int) -> int:
+def sum(x: int, y: int) -> float:
     return x + y
 
 
@@ -6,5 +6,18 @@ def calculateTax(bill: float, tax_rate: float) -> float:
     return round((bill * tax_rate) / 100.00, 2)
 
 
-print(sum(10, 13))
-print("total tax:", calculateTax(175.00, 15))
+# Global scope
+my_global: int = 10
+
+
+def fn1():
+    local_v = 10
+    print("Access to global", my_global)
+
+    def fn2():
+        print("Access to enclosed", local_v)
+
+    fn2()
+
+
+fn1()
