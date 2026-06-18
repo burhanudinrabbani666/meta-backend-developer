9f6c158c4f94d46167670a6565182933aa2465aa
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-18 16:05:15 +0700

    dict

---
+++ 02-programming-in-python/dict.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+user: dict[str, Any] = {
+    "first_name": "burhanudin",
+    "last_name": "rabbani",
+    "email": "bani@bani.io",
+    "age": 23,
+    "username": "bani1234",
+}
+
+print(type(user))
+print(f"first name: {user['first_name']}")
+print(f"last name: {user['last_name']}")
+print(f"username: {user['username']}")
+print(f"email: {user['email']}")
+
+user["username"] = "bani son of dragon"
